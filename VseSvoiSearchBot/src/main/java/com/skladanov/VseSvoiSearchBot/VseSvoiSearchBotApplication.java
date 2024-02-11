57088e70bfc38fd8cda9a1bc638d7fe8fb3e62f5
Kirill Skladanov <102518082+KirSkl@users.noreply.github.com>
2024-02-11 17:58:40 +0300

    initialization
    Добавлен базовый Spring Boot проект

---
+++ VseSvoiSearchBot/src/main/java/com/skladanov/VseSvoiSearchBot/VseSvoiSearchBotApplication.java
@@ -0,0 +1,13 @@
+package com.skladanov.VseSvoiSearchBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VseSvoiSearchBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VseSvoiSearchBotApplication.class, args);
+	}
+
+}
---
+++ VseSvoiSearchBot/src/test/java/com/skladanov/VseSvoiSearchBot/VseSvoiSearchBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skladanov.VseSvoiSearchBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VseSvoiSearchBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
