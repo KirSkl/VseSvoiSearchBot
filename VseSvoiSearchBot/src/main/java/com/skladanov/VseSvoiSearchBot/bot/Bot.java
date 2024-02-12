716d264c990d19f385c88a70638765fce837c32f
Kirill Skladanov <wra1th@inbox.ru>
2024-02-12 17:33:02 +0300

    Создать работоспособную заготовку бота

---
+++ VseSvoiSearchBot/src/main/java/com/skladanov/VseSvoiSearchBot/bot/Bot.java
@@ -0,0 +1,74 @@
+package com.skladanov.VseSvoiSearchBot.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Slf4j
+public class Bot extends TelegramLongPollingBot {
+    private static final String START = "/start";
+
+    public Bot(@Value("${bot.token}") String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
+            return;
+        }
+        String message = update.getMessage().getText();
+        Long chatId = update.getMessage().getChatId();
+        switch (message) {
+            case START -> {
+                String userName = update.getMessage().getChat().getUserName();
+                startCommand(chatId, userName);
+            }
+            /*case USD -> usdCommand(chatId);
+            case EUR -> eurCommand(chatId);
+            case HELP -> helpCommand(chatId);*/
+            default -> unknownCommand(chatId);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "VSSearch_Bot";
+    }
+
+    private void startCommand(Long chatId, String userName) {
+        var text = """
+                Добро пожаловать в бот, %s!
+                
+                Здесь Вы сможете найти специалиста из сообщества "Все свои" под свой запрос.
+                
+                Для этого воспользуйтесь командами:
+              
+                
+                Дополнительные команды:
+                /help - получение справки
+                """;
+        var formattedText = String.format(text, userName);
+        sendMessage(chatId, formattedText);
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        var chatIdStr = String.valueOf(chatId);
+        var sendMessage = new SendMessage(chatIdStr, text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Ошибка отправки сообщения", e);
+        }
+    }
+
+    private void unknownCommand(Long chatId) {
+        var text = "Не удалось распознать команду!";
+        sendMessage(chatId, text);
+    }
+}
