92bcccad1857a2b5c6cff12ccb18c952f27963df
Kirill Skladanov <wra1th@inbox.ru>
2024-02-12 18:58:20 +0300

    <feat> реализовать отправку сообщений в тест-группу по запросу

--- VseSvoiSearchBot/src/main/java/com/skladanov/VseSvoiSearchBot/bot/Bot.java
+++ VseSvoiSearchBot/src/main/java/com/skladanov/VseSvoiSearchBot/bot/Bot.java
@@ -12,6 +12,8 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 @Slf4j
 public class Bot extends TelegramLongPollingBot {
     private static final String START = "/start";
+    private static final String REQUEST = "/request";
+    private static final Long TEST_CHAT_ID = -1002107664061L;
 
     public Bot(@Value("${bot.token}") String botToken) {
         super(botToken);
@@ -28,6 +30,9 @@ public class Bot extends TelegramLongPollingBot {
             case START -> {
                 String userName = update.getMessage().getChat().getUserName();
                 startCommand(chatId, userName);
+            }
+            case REQUEST -> {
+                requestCommand(chatId, message);
             }
             /*case USD -> usdCommand(chatId);
             case EUR -> eurCommand(chatId);
@@ -46,15 +51,20 @@ public class Bot extends TelegramLongPollingBot {
                 Добро пожаловать в бот, %s!
                 
                 Здесь Вы сможете найти специалиста из сообщества "Все свои" под свой запрос.
-                
+                А еще вы очень красивая, вы знали?
                 Для этого воспользуйтесь командами:
-              
+                /request - вместе с командой напишите ваш запрос
                 
                 Дополнительные команды:
                 /help - получение справки
                 """;
         var formattedText = String.format(text, userName);
         sendMessage(chatId, formattedText);
+    }
+
+    private void requestCommand(Long chatId, String message) {
+        sendMessage(TEST_CHAT_ID, message + "вот так запрос");
+        sendMessage(chatId, "Ваш запрос отправлен: " + message + "вот так запрос" );
     }
 
     private void sendMessage(Long chatId, String text) {
