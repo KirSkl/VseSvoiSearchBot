3b746d0f7f36624e48f15623085bae1ad60c10fa
Kirill Skladanov <wra1th@inbox.ru>
2024-02-15 21:36:07 +0300

    <refactor/feat> обработка апдейтов вынесена в отдельный класс. Написана структура составления запроса

--- VseSvoiSearchBot/src/main/java/com/skladanov/VseSvoiSearchBot/bot/Bot.java
+++ VseSvoiSearchBot/src/main/java/com/skladanov/VseSvoiSearchBot/bot/Bot.java
@@ -14,9 +14,11 @@ public class Bot extends TelegramLongPollingBot {
     private static final String START = "/start";
     private static final String REQUEST = "/request";
     private static final Long TEST_CHAT_ID = -1002107664061L;
+    private final UpdateHandler updateHandler;
 
-    public Bot(@Value("${bot.token}") String botToken) {
+    public Bot(@Value("${bot.token}") String botToken, UpdateHandler updateHandler) {
         super(botToken);
+        this.updateHandler = updateHandler;
     }
 
     @Override
@@ -24,21 +26,7 @@ public class Bot extends TelegramLongPollingBot {
         if (!update.hasMessage() || !update.getMessage().hasText()) {
             return;
         }
-        String message = update.getMessage().getText();
-        Long chatId = update.getMessage().getChatId();
-        switch (message) {
-            case START -> {
-                String userName = update.getMessage().getChat().getUserName();
-                startCommand(chatId, userName);
-            }
-            case REQUEST -> {
-                requestCommand(chatId, message);
-            }
-            /*case USD -> usdCommand(chatId);
-            case EUR -> eurCommand(chatId);
-            case HELP -> helpCommand(chatId);*/
-            default -> unknownCommand(chatId);
-        }
+        sendMessage(updateHandler.handleUpdate(update));
     }
 
     @Override
@@ -46,39 +34,12 @@ public class Bot extends TelegramLongPollingBot {
         return "VSSearch_Bot";
     }
 
-    private void startCommand(Long chatId, String userName) {
-        var text = """
-                Добро пожаловать в бот, %s!
-                
-                Здесь Вы сможете найти специалиста из сообщества "Все свои" под свой запрос.
-                А еще вы очень красивая, вы знали?
-                Для этого воспользуйтесь командами:
-                /request - вместе с командой напишите ваш запрос
-                
-                Дополнительные команды:
-                /help - получение справки
-                """;
-        var formattedText = String.format(text, userName);
-        sendMessage(chatId, formattedText);
-    }
-
-    private void requestCommand(Long chatId, String message) {
-        sendMessage(TEST_CHAT_ID, message + "вот так запрос");
-        sendMessage(chatId, "Ваш запрос отправлен: " + message + "вот так запрос" );
-    }
-
-    private void sendMessage(Long chatId, String text) {
-        var chatIdStr = String.valueOf(chatId);
-        var sendMessage = new SendMessage(chatIdStr, text);
+    private void sendMessage(SendMessage message) {
         try {
-            execute(sendMessage);
+            execute(message);
         } catch (TelegramApiException e) {
             log.error("Ошибка отправки сообщения", e);
         }
     }
 
-    private void unknownCommand(Long chatId) {
-        var text = "Не удалось распознать команду!";
-        sendMessage(chatId, text);
-    }
 }
