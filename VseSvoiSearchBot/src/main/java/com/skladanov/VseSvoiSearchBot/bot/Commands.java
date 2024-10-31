253238f58e8b58baf4f9a473fe98722870fc6238
Kirill Skladanov <wra1th@inbox.ru>
2024-10-31 19:16:45 +0300

    <refactor> вынести команды в отдельный класс

---
+++ VseSvoiSearchBot/src/main/java/com/skladanov/VseSvoiSearchBot/bot/Commands.java
@@ -0,0 +1,10 @@
+package com.skladanov.VseSvoiSearchBot.bot;
+
+public class Commands {
+    public static final String DELETE_RESPONSE = "/delete_resp";
+    public static final String HELP = "/help";
+    public static final String REQUEST = "/request";
+    public static final String DELETE_REQUEST = "/delete_req";
+    public static final String BACK = "/back";
+    public static final String RESPONSE = "/response";
+}
